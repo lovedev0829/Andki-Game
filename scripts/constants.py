c2af7cd9dacf03c73080207517e3e5635d5ed1b1
namelless <namelless0k@gmail.com>
2024-07-11 06:43:06 +0200

    added function to display trainers randomly

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,4 +1,4 @@
 import os
 cwd = os.path.dirname(__file__)
 anki_data_path = os.path.join(cwd, "anki_data.json")
-TRAINERS = ['assets/trainer.png']
+TRAINERS = [os.path.join('assets/trainers/',name) for name in os.listdir('assets/trainers/')]
