c82f02ba6b5e785cb0f3185d03cc9efd72e013dc
namelless <namelless0k@gmail.com>
2024-07-16 15:41:30 +0200

    fixed issue with the integrated ui

--- __init__.py
+++ __init__.py
@@ -31,10 +31,10 @@ def bridge(handled, message: str, context):
     global started
     if message == "start_rpg":
         if not started:
-            threading.Thread(target=start_rpg, daemon=True).start()
+            start_rpg()
             started = True
     if message == 'rerender':
-        rerender()
+        pass
     if message in ["ease1", "ease2", "ease3", "ease4"]:
         add_msg_to_db(message)
     return handled
@@ -44,7 +44,7 @@ aqt.gui_hooks.overview_will_render_content.append(add_btn)
 aqt.gui_hooks.webview_did_receive_js_message.append(bridge)
 # aqt.gui_hooks.
 # Add a button to the main screen with title "start game" and that starts the game when clicked
-pygame_surf = None
+
 def start_game():
     global pygame_instace
     pygame_instace = streakgame.main.mainnowin(stats)
@@ -54,7 +54,6 @@ def start_rpg():
     global pygame_instace
     mw.win = win = QMainWindow()
     ui = attribute_popup(win)
-    main.game = None
 def on_profile_open():
     due_tree = mw.col.sched.deck_due_tree()
     to_review = due_tree.review_count + due_tree.learn_count + due_tree.new_count
@@ -70,9 +69,6 @@ def on_profile_open():
     except pygame.error as e:
         print(e)
 
-q = threading.Thread(start_rpg, daemon=True)
-
-pygame_instace = None
 gui_hooks.profile_did_open.append(on_profile_open)
 gui_hooks.reviewer_did_answer_card.append(process_file)
 
@@ -84,40 +80,13 @@ mw.form.menuTools.addAction(action)
 rpg = aqt.qt.QAction("Start rpg", mw)
 rpg.triggered.connect(start_rpg)
 mw.form.menuTools.addAction(rpg)
-web = None
-def rerender():
-    addon_name = mw.addonManager.addonFromModule(__name__)
-    pos = mw.app.primaryScreen().size().width()/2 - stats.SIZE[0]/2
-    buf = 5
-    # path = f"/_addons/{addon_name}/assets/temp/{main.Frame}.png"
-    
-    css = f"""    <style>
-        .container {{
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }}
-    </style>        """
-    global web
-    web.body += (
-        f'''
-{css}
-<div class="container">
-<img src="image.png" margin id="foo";">
-</div>
-<script>
-document.getElementById("foo").src = '/_addons/{addon_name}/assets/temp/{stats.Frame}.png';
-pycmd('rerender');
-</script>''')
-    
+
 
 def on_webview_will_set_content(
     web_content: WebContent, context: object | None
 ) -> None:
     if not isinstance(context, DeckBrowser):
         return
-    global web
-    web = web_content
     addon_name = mw.addonManager.addonFromModule(__name__)
     pos = mw.app.primaryScreen().size().width()/2 - stats.SIZE[0]/2
     buf = 5
@@ -125,8 +94,7 @@ def on_webview_will_set_content(
     css = f"""    <style>
         .container {{
             display: flex;
-            justify-content: center;
-            align-items: center;
+            left: {pos};
         }}
     </style>        """
     web_content.body += (
@@ -145,6 +113,7 @@ setInterval(() => {{
         document.getElementById("foo").src = '/_addons/{addon_name}/assets/temp/'+Math.floor(counter/{buf})*{buf}+'.png'; 
     }}
     counter += 0.9;
+    counter = Math.max(counter, {stats.Frame + 5});
 }}, 1100/60);
 </script>''')
 
