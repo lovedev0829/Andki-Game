4b6bcea12e271ae71cbc8bb72d1ab382c8d17f66
namelless <namelless0k@gmail.com>
2024-09-13 12:04:05 +0200

    added stuff

---
+++ streakgame/assets/map/tilemaps/grass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="grass" tilewidth="32" tileheight="32" tilecount="125" columns="25">
+ <image source="../tilemaps_png/new/grass.png" width="800" height="160"/>
+</tileset>
